f3a905c9b70b2623fd3bc26ddb0f36c9f05292e5
Damian <damian.miara93@gmail.com>
2018-03-30 12:55:42 +0200

    Basic placeholder

--- app/views/Default/Views/index.js
+++ app/views/Default/Views/index.js
@@ -6,6 +6,12 @@ import './styles.scss';
 const Views = (props) => (
   <div>
     <h2>Main page</h2>
+	<h3>Wybierz swoje kino</h3>
+	<form>
+	  <select>
+		<option value="placeholder">Placeholder</option>
+      </select>
+	</form>
   </div>
 );
 
